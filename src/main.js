f329e2ce6e5cd2ce3b5db2e43bb0cdae0398be63
Facundo Matteo <facumatteo@gmail.com>
2017-03-19 14:24:26 -0300

    First version of Redis store

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import redis from 'redis'
+import bluebird from 'bluebird'
+
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+export default function RedisStore(options) {
+  const client = redis.createClient(options)
+  return {
+    async get(key) {
+      const elem = await client.getAsync(key)
+      if (!elem) {
+        return null
+      }
+      const nextDate = await client.pttlAsync(key)
+      return {
+        count: Number(elem),
+        nextDate: (new Date()).getTime() + nextDate,
+      }
+    },
+    async set(key, count, expire) {
+      await client.setAsync(key, String(count))
+      await client.pexpireAsync(key, expire)
+    },
+  }
+}
